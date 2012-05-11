0e00694d7fa1edba50b33deaa4b7d27b28fe152e
Niels van Hoorn <niels@brainsley.com>
2012-05-11 15:20:35 +0200

    Making the preprocessor work for assets:precompile in production

--- lib/sprockets/engine.rb
+++ lib/sprockets/engine.rb
@@ -1,9 +1,9 @@
 if defined?(Rails)
   module Sprockets
     class CommonJSEngine < Rails::Engine
-      config.after_initialize do
-        Rails.application.assets.register_postprocessor 'application/javascript', CommonJS
-        Rails.application.assets.append_path File.expand_path('../..', __FILE__)
+      initializer :setup_closure do |app|
+        app.assets.register_postprocessor 'application/javascript', CommonJS
+        app.assets.append_path File.expand_path('../..', __FILE__)
       end
     end
   end
