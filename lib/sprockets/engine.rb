a26890fb825f2697954710f4011ab770523d4da4
Niels van Hoorn <niels@brainsley.com>
2012-05-11 15:33:51 +0200

    Moving commonjs.js to assets
    By moving commonjs.js to /lib/assets/, we don't need to append the path manually, and the .rb files don't get pre	compiled anymore

--- lib/sprockets/engine.rb
+++ lib/sprockets/engine.rb
@@ -3,7 +3,6 @@ if defined?(Rails)
     class CommonJSEngine < Rails::Engine
       initializer :setup_closure do |app|
         app.assets.register_postprocessor 'application/javascript', CommonJS
-        app.assets.append_path File.expand_path('../..', __FILE__)
       end
     end
   end
