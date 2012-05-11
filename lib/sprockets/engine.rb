98ad29621ca21a96d5f7e8a650b389e660c73847
Niels van Hoorn <niels@brainsley.com>
2012-05-11 16:23:07 +0200

    Oops.. a bit to much copy-pasting

--- lib/sprockets/engine.rb
+++ lib/sprockets/engine.rb
@@ -1,7 +1,7 @@
 if defined?(Rails)
   module Sprockets
     class CommonJSEngine < Rails::Engine
-      initializer :setup_closure do |app|
+      initializer :setup_commonjs do |app|
         app.assets.register_postprocessor 'application/javascript', CommonJS
       end
     end
